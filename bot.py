ec9918335f2e86d39cee141314ab7dc25e365b41
Terry Qiu <terryq2@illinois.edu>
2025-06-26 16:23:02 +0800

    Abstracted minimax and minor code quality improvements

--- bot.py
+++ bot.py
@@ -1,81 +1,10 @@
-from utilities import Candidate, Frame
-from stones import Stone
-from board import Board
+from strategies import minimax
 
 
 class Bot:
-    def minimax_iterative(self, max_depth: int, board: Board) -> int:
-            if board.current_player == Stone.WHITE:
-                initial_candidates = board.candidates_manager.deep_copy_candidates(board.candidates_manager.candidates_added_white)
-            else:
-                initial_candidates = board.candidates_manager.deep_copy_candidates(board.candidates_manager.candidates_added_black) 
-            
-            # if not initial_candidates:
-            #     initial_candidates = [Candidate(board.move_stack[-1].point.x+1, board.move_stack[-1].point.y, Stone.BLACK)]
-                
-            call_stack: list[Frame] = [Frame(0, board.current_player, 0, initial_candidates, None)]
-            while call_stack:
-                current_frame = call_stack[-1]
-
-                if current_frame.candidate_index > 0:
-                    board.cancel()
-
-                if current_frame.depth == max_depth:
-                    score = board.evaluate_board()
-                    call_stack.pop()
-                    if not call_stack:
-                        return score
-                    
-                    parent_frame = call_stack[-1]
-                    if parent_frame.player == Stone.WHITE and score > parent_frame.best_score:
-                        parent_frame.best_score = score
-                        parent_frame.best_candidate = Candidate(current_frame.current_candidate.point.x, current_frame.current_candidate.point.y, current_frame.current_candidate.player)
-
-                    if parent_frame.player == Stone.BLACK and score < parent_frame.best_score:
-                        parent_frame.best_score = score
-                        parent_frame.best_candidate = Candidate(current_frame.current_candidate.point.x, current_frame.current_candidate.point.y, current_frame.current_candidate.player)
+    def __init__(self):
+        self.minimax: minimax.Minimax = minimax.Minimax()
 
 
-                    continue
-
-                if current_frame.candidate_index >= len(current_frame.candidates):
-                    best_score = current_frame.best_score
-                    best_move = current_frame.best_candidate
-
-                    call_stack.pop()
-
-                    if not call_stack:
-                        return (best_score, best_move)
-                    
-                    parent_frame = call_stack[-1]
-
-                    if parent_frame.player == Stone.WHITE and best_score > parent_frame.best_score:
-                        parent_frame.best_score = best_score
-                        parent_frame.best_candidate = Candidate(current_frame.current_candidate.point.x, current_frame.current_candidate.point.y, current_frame.current_candidate.player)
-
-                    if parent_frame.player == Stone.BLACK and best_score < parent_frame.best_score:
-                        parent_frame.best_score = best_score
-                        parent_frame.best_candidate = Candidate(current_frame.current_candidate.point.x, current_frame.current_candidate.point.y, current_frame.current_candidate.player)
-
-
-                    continue
-
-                candidate = current_frame.candidates[current_frame.candidate_index]
-                current_frame.candidate_index += 1
-                board.place(candidate.point.x, candidate.point.y)
-        
-                
-                next_player = Stone(current_frame.player * -1)
-                if next_player == Stone.WHITE:
-                    next_candidates = board.candidates_manager.deep_copy_candidates(board.candidates_manager.candidates_added_white) 
-                else:
-                    next_candidates = board.candidates_manager.deep_copy_candidates(board.candidates_manager.candidates_added_black) 
-                
-                call_stack.append(Frame(
-                    depth=current_frame.depth + 1,
-                    player=next_player,
-                    candidate_index=0,
-                    candidates=next_candidates,
-                    current_candidate= Candidate(candidate.point.x, candidate.point.y, candidate.player)
-                ))
-                
+if __name__ == "__main__":
+    bot: Bot = Bot()
---
+++ strategies/minimax.py
@@ -0,0 +1,111 @@
+from utilities import Candidate, Frame, Move
+from stones import Stone
+from board import Board
+
+
+class Minimax:
+    """Class that contains the minimax strategy"""
+    def run(self, max_depth: int, board: Board) -> tuple[int, Move]:
+        """Minimax function that returns"""
+        initial_candidates = self.get_candidate(board, board.current_player)
+        call_stack: list[Frame] = [Frame(0, board.current_player, 0, initial_candidates, None)]
+        while call_stack:
+            current_frame = call_stack[-1]
+
+            if current_frame.candidate_index > 0:
+                board.cancel()
+
+            if current_frame.depth == max_depth:
+                if self.handle_max_depth_frame(board, current_frame, call_stack):
+                    raise Exception('Please set your the max depth to be greater than or equal to 1')
+                continue
+
+            if current_frame.candidate_index >= len(current_frame.candidates):
+                if self.handle_terminal_frame(current_frame, call_stack):
+                    return (current_frame.best_score, current_frame.best_candidate)
+                continue
+
+            candidate = self.place_next_candidate(board, current_frame)
+
+            next_player = Stone(current_frame.player * -1)
+
+            next_candidates = self.get_candidate(board, next_player)
+
+            next_frame = Frame(
+                depth=current_frame.depth + 1,
+                player=next_player,
+                candidate_index=0,
+                candidates=next_candidates,
+                current_candidate= Candidate(candidate.point.x, candidate.point.y, candidate.player)
+            )
+            self.add_new_frame(next_frame, call_stack)
+
+    def handle_terminal_frame(self, frame: Frame, call_stack: list[Frame]) -> bool:
+        """Handles the case when we traversed all candidates of a frame, returns true if there exists no more frames and we are done"""
+        if frame.candidate_index >= len(frame.candidates):
+            best_score = frame.best_score
+
+            call_stack.pop()
+
+            if not call_stack:
+                return True
+            
+            parent_frame = call_stack[-1]
+
+            if parent_frame.player == Stone.WHITE and best_score > parent_frame.best_score:
+                parent_frame.best_score = best_score
+                parent_frame.best_candidate = Candidate(frame.current_candidate.point.x, frame.current_candidate.point.y, frame.current_candidate.player)
+
+            if parent_frame.player == Stone.BLACK and best_score < parent_frame.best_score:
+                parent_frame.best_score = best_score
+                parent_frame.best_candidate = Candidate(frame.current_candidate.point.x, frame.current_candidate.point.y, frame.current_candidate.player)
+
+            return False
+        
+    def handle_max_depth_frame(self, board: Board, frame: Frame, call_stack: list[Frame]) -> bool:
+        """Handles the case when we reached maximum recursion depth, returns true if there are no more frames and we are done"""
+        score = board.evaluate_board()
+
+        call_stack.pop()
+
+        if not call_stack:
+            return True
+        
+        parent_frame = call_stack[-1]
+
+        if parent_frame.player == Stone.WHITE and score > parent_frame.best_score:
+            parent_frame.best_score = score
+            parent_frame.best_candidate = Candidate(frame.current_candidate.point.x, frame.current_candidate.point.y, frame.current_candidate.player)
+
+        if parent_frame.player == Stone.BLACK and score < parent_frame.best_score:
+            parent_frame.best_score = score
+            parent_frame.best_candidate = Candidate(frame.current_candidate.point.x, frame.current_candidate.point.y, frame.current_candidate.player)
+
+        return False
+    
+    def place_next_candidate(self, board: Board, frame: Frame) -> Candidate:
+        """Places the current candidate for the current frame on the board, returns the candidate being placed"""
+        candidate = frame.candidates[frame.candidate_index]
+
+        frame.candidate_index += 1
+
+        board.place(candidate.point.x, candidate.point.y)
+
+        return candidate
+
+    def add_new_frame(self, frame: Frame, call_stack: list[Frame]):
+        """Appends the frame onto the call stack"""
+        call_stack.append(frame)
+    
+    def get_candidate(self, board: Board, player: Stone):
+        """Returns a deepcopy of the candidates of the player entered"""
+        if player == Stone.WHITE:
+            candidates = board.candidates_manager.deep_copy(board.candidates_manager.candidates_added_white) 
+        else:
+            candidates = board.candidates_manager.deep_copy(board.candidates_manager.candidates_added_black)
+
+        if candidates is None:
+            raise Exception('Check player assignment')
+        
+        return candidates
+                
